e60662b414934d2cf60c4ed1822f1969ef0d3517
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-21 01:14:15 +0300

    Adding startGame func to detect touching boundaries of eventListener

--- game.js
+++ game.js
@@ -8,5 +8,32 @@ window.onload= function(){
 	
 	let boundaries = document.getElementsByClassName("boundary");
 	
-
+	function startGame()
+	{
+		console.log('game started');
+	
+		boundaries[0].onmouseover=function()
+			{
+				setLose();
+			}			
+		boundaries[1].onmouseover=function()
+			{
+				setLose();	
+			}			
+		boundaries[2].onmouseover=function()
+			{
+				setLose();
+			}			
+		boundaries[3].onmouseover=function()
+			{
+				setLose();		
+			}
+		boundaries[4].onmouseover=function()
+			{
+				setLose();
+			}
+						
+			
+		
+	}
 }
