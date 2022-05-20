da736cd88a7cea959e6d7b98cd590a9b8a33d374
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-21 01:12:59 +0300

    Adding event listener on div of id start

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
 window.onload= function(){
+	
+	let start=document.getElementById("start");
+	
+	//make sure start is not null variable
+	if(start)
+		start.addEventListener('click',startGame);
+	
+	let boundaries = document.getElementsByClassName("boundary");
+	
 
 }
